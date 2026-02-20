a6e83031568fcf238e74d7036a42598e9493f396
Wilson-Flores <willyflores173@gmail.com>
2026-02-20 10:27:43 -0500

    uncommented lines.

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,36 @@
 
 
 int main() {
-    // RomManager rom_manager("../roms");
-    //
-    // if (!rom_manager.verifyRequiredRoms()) {
-    //     std::cerr << "Please make sure all ROM files are in the 'roms/' folder.\n";
-    //     return 1;
-    // }
-    //
-    // // Load roms into memory
-    // std::unordered_map<std::string, std::vector<uint8_t>> roms = rom_manager.loadRoms();
-    //
-    // // dump roms into txt files
-    // dumpRomsForDebug(roms);
-    //
-    // // clear rom_dumps folder
-    // // clearRomDumpFolder();
-    //
-    //
-    // // Combine the core rom files
-    // const std::vector<std::string> pacman_rom_order = {
-    //     "pacman.6e",
-    //     "pacman.6f",
-    //     "pacman.6h",
-    //     "pacman.6j"
-    // };
-    //
-    // concatenateRoms(roms, pacman_rom_order, "../roms/pacman_program.rom");
-    // convertCombinedRomToText(
-    //     "../roms/pacman_program.rom",
-    //     "../rom_dumps/pacman_program.txt"
-    // );
+    RomManager rom_manager("../roms");
+
+    if (!rom_manager.verifyRequiredRoms()) {
+        std::cerr << "Please make sure all ROM files are in the 'roms/' folder.\n";
+        return 1;
+    }
+
+    // Load roms into memory
+    std::unordered_map<std::string, std::vector<uint8_t>> roms = rom_manager.loadRoms();
+
+    // dump roms into txt files
+    dumpRomsForDebug(roms);
+
+    // clear rom_dumps folder
+    // clearRomDumpFolder();
+
+
+    // Combine the core rom files
+    const std::vector<std::string> pacman_rom_order = {
+        "pacman.6e",
+        "pacman.6f",
+        "pacman.6h",
+        "pacman.6j"
+    };
+
+    concatenateRoms(roms, pacman_rom_order, "../roms/pacman_program.rom");
+    convertCombinedRomToText(
+        "../roms/pacman_program.rom",
+        "../rom_dumps/pacman_program.txt"
+    );
 
     // convert combined rom file into asm file
     std::vector<uint8_t> pacman_rom = loadRomFile("../roms/pacman_program.rom");
